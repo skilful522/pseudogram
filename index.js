d7e6dfa2c294d8a6e340c572ff0561ff6b2022a1
Stanislav Sergienya <skilful522@gmail.com>
2019-07-28 18:53:26 +0300

    reg added, photoloader added

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 var login = document.querySelector("#login");
-var dialog = document.querySelector("dialog");
-var close = document.querySelector("#close");
+var signIn = document.querySelector("#sign-in");
+
+var loginDialog = document.querySelector("#loginDialog");
+var signInDialog = document.querySelector("#signInDialog");
+
+var closeLoginDialog = document.querySelector("#closeLoginDialog");
+var closeSingInDialog = document.querySelector("#closeSingInDialog");
+
+var ok = document.querySelector("#ok");
+
+var userFirstNameElement = document.querySelector('#userFirstName');
+var userLastNameElement = document.querySelector('#userLastName');
+var userEmailElement = document.querySelector("#userEmail");
+
+var token;
+var API = "https://intern-staging.herokuapp.com/api";
+
+var userNameElement = document.getElementById("userName");
+var registration = document.querySelector("#registration");
+
 var save = document.querySelector("#loginButton");
 var search = document.querySelector("#search");
 var post = document.querySelector("#postsButton");
 var tagged = document.querySelector("#taggedButton");
 var oldImage = document.getElementsByClassName("img");
 var images = document.querySelector("#images");
+var edit = document.querySelector("#editButton");
+var feedButton = document.querySelector("#feedButton");
+
+var choosePhotoForm = document.forms.namedItem("choosePhotoForm");
 
 var userLogin = document.querySelector("#userLogin");
 var userPassword = document.querySelector("#userPassword");
@@ -17,6 +39,81 @@ var authorized = false;
 
 var infoBlock = document.querySelector(".infoBlock");
 var res = [];
+
+var userName;
+var usersNames = [];
+var usersEmails = [];
+
+var fragment = document.createDocumentFragment();
+
+var routes = {
+    "login": {
+        html: "login/login.html",
+    }
+};
+
+var requestTemplate = (function () {
+    var cache = {};
+    return function (url) {
+        if (cache.hasOwnProperty(url)) {
+            return Promise.resolve(cache[url]);
+        } else {
+            return fetch(url).then(function (res) {
+                return res.text();
+            }).then(function (html) {
+                cache[url] = html;
+                return html;
+            })
+        }
+    }
+})();
+
+var runScript = (function () {
+    var cache = {};
+    return function (src) {
+        if (cache.hasOwnProperty(src)) {
+            cache[src]();
+        } else {
+            import(src).then(function (module) {
+                cache[src] = module.default;
+                cache[src]();
+            }).catch(function (err) {
+                console.error(err)
+            });
+        }
+    }
+})();
+
+
+var render = (function () {
+    var content = document.getElementsByClassName('infoBlock');
+    return function (html) {
+        content.innerHTML = html;
+    }
+})();
+
+var handleRouting = (function () {
+    var previousHash;
+    return function () {
+        var hash = window.location.hash.split('#/')[1] || '';
+        if (previousHash === hash) {
+            return;
+        }
+        if (routes.hasOwnProperty(hash)) {
+            previousHash = hash;
+            var urls = routes[hash];
+            requestTemplate(urls.html).then(function (html) {
+                render(html);
+                if (urls.hasOwnProperty('src')) {
+                    runScript(urls.src);
+                }
+            })
+        }
+    }
+})();
+
+window.addEventListener('DOMContentLoaded', handleRouting);
+window.addEventListener('hashchange', handleRouting);
 
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -30,6 +127,7 @@ res[4] = "360x360";
 res[5] = "340x340";
 
 search.style.visibility = "hidden";
+feedButton.style.display = "none";
 infoBlock.style.display = "none";
 
 images.addEventListener("click", function (event) {
@@ -60,7 +158,7 @@ search.onkeypress = function (event) {
     if (event.keyCode == 13) {
         for (let i = 0; i < res.length; i++) {
             oldImage[i].style.visibility = "visible";
-            image = "https://source.unsplash.com/" + res[i] + "/weekly?" + searchText;
+            var image = "https://source.unsplash.com/" + res[i] + "/weekly?" + searchText;
             oldImage[i].src = image;
         }
     }
@@ -75,7 +173,7 @@ tagged.addEventListener("click", function () {
 
 post.addEventListener("click", function () {
     for (let i = 0; i < res.length; i++) {
-        image = "https://source.unsplash.com/320x320/weekly?" + getRandomArbitrary(0,200);
+        var image = "https://source.unsplash.com/320x320/weekly?" + getRandomArbitrary(0, 200);
         oldImage[i].style.visibility = "visible";
         oldImage[i].src = image;
     }
@@ -84,37 +182,121 @@ post.addEventListener("click", function () {
 login.addEventListener("click", function () {
     if (login.innerHTML === "Log out") {
         authorized = false;
+        feedButton.style.display = "none";
         infoBlock.style.display = "none";
+        signIn.style.display = "block";
         login.innerHTML = "Log in";
         search.style.visibility = "hidden";
     } else {
-        dialog.showModal();
+        loginDialog.showModal();
     }
 });
 
-close.addEventListener("click", function () {
-    dialog.close();
+function getUserName() {
+    return userNameElement.innerText = userFirstNameElement.value + " " + userLastNameElement.value;
+}
+
+function setUsersNamesInBase() {
+    userName = getUserName();
+    usersNames.push(userName + " " + getUserEmail());
+}
+
+function getUserEmail() {
+    usersEmails = userEmailElement.innerText;
+    usersEmails = usersEmails.split(" ");
+    return userEmailElement.innerText = userEmailElement.value;
+}
+
+var click = 0;
+
+ok.addEventListener('click', function () {
+    registration.innerHTML = "Registration";
+    click += 1;
+    if (click === 1) {
+        getUserName();
+        setUsersNamesInBase();
+        userFirstNameElement.style.display = "none";
+        userLastNameElement.style.display = "none";
+        userEmailElement.style.display = "none";
+        registration.innerHTML = "Check your email";
+    }
+    if (click === 2) {
+        click = 0;
+        userFirstNameElement.style.display = "block";
+        userLastNameElement.style.display = "block";
+        userEmailElement.style.display = "block";
+        var signInReq = {email: getUserEmail()};
+        doSignInRequest(signInReq);
+        signInDialog.close();
+        images.style.visibility = "visible";
+    }
+});
+
+
+
+closeLoginDialog.addEventListener("click", function () {
+    loginDialog.close();
     userLogin.value = "";
     userPassword.value = "";
+});
+
+closeSingInDialog.addEventListener("click", function () {
+    signInDialog.close();
 });
 
 save.addEventListener("click", function () {
-
     if (userLogin.value == myLogin && userPassword.value == myPassword) {
-        var singInReq = {email: userLogin.value, password: userPassword.value};
-        doRequest(singInReq);
+        var logInReq = {email: userLogin.value, password: userPassword.value};
+        doLogInRequest(logInReq);
         authorized = true;
+        signIn.style.display = "none";
+        feedButton.style.display = "block";
         infoBlock.style.display = "block";
         login.innerHTML = "Log out";
         search.style.visibility = "visible";
+        loginDialog.close();
+        userLogin.value = "";
+        userPassword.value = "";
     }
-    dialog.close();
-    userLogin.value = "";
-    userPassword.value = "";
 });
 
-function doRequest(data) {
+feedButton.addEventListener('click', function () {
+    for (var i = 0; i < 3; i++) {
+        var img = document.createElement("img");
+        img.src = "https://source.unsplash.com/320x320/weekly?" + getRandomArbitrary(0, 200);
+        img.className = 'feedImages';
+        fragment.appendChild(img);
+        images.appendChild(fragment);
+    }
+});
+
+/*edit.addEventListener('click', function () {
+    const elements = document.getElementsByClassName("feedImages");
+    while (elements.length > 0) elements[0].remove();
+    console.log(usersNames.length);
+    console.log(images);
+}); */
+
+signIn.addEventListener('click', function () {
+    signInDialog.showModal();
+});
+
+function doLogInRequest(data) {
     fetch('https://intern-staging.herokuapp.com/api/identification/sign_in', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then(
+        resp => resp.json()
+    ).then(
+        json => token = json
+    );
+}
+
+function doSignInRequest(data) {
+    fetch('https://intern-staging.herokuapp.com/api/identification', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
@@ -126,3 +308,27 @@ function doRequest(data) {
         json => console.log(json)
     );
 }
+function doImageRequest(url, method, data, headers) {
+    fetch("${API}${url}", {
+        method: method,
+        body: data,
+        headers: headers,
+    }).then(
+        resp => resp.json()
+    ).then(
+        json => console.log(json)
+    );
+}
+
+doImageRequest('/identification','GET',null)
+
+choosePhotoForm.addEventListener('submit', function (event) {
+    var formD = new FormData(this);
+    console.log(token);
+    formD.append('parentEntityId', '1');
+    doImageRequest("/file",'POST', formD, {"token": token});
+    event.preventDefault();
+});
+
+
+
--- server.js
+++ server.js
@@ -1,26 +1,57 @@
 const http = require('http');
 const fs = require('fs');
-const url = require('url');
+const path = require('path');
 
 const port = process.env.PORT || 3000;
 
-const readFile = (url, response) => {
-    fs.readFile(url, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            response.statusCode = 204;
-            response.end(JSON.stringify(err));
-        } else {
-            console.log(url);
-            response.writeHead(200);
-            response.end(data);
+const server = http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    let filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    let contentType = 'text/html';
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.svg': 'application/image/svg+xml'
+    };
+
+    contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            console.log('error', error);
+            if(error.code == 'ENOENT'){
+                response.writeHead(404, { 'Content-Type': contentType });
+                response.end();
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
         }
     });
-};
 
-const server = http.createServer(function (request, response) {
-    const path = url.parse(request.url, true).pathname.replace(/^\/+|\/+$/g, '');
-    readFile(path, response);
 });
 
 server.listen(port);
